49f1c0c68e1ef790b11700783313176db4be619b
yosuke.yomosue <stbrimaha@gmail.com>
2021-08-29 15:38:17 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,21 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show]
   
   def index
-    @tasks = Task.all
+    @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -55,5 +56,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
